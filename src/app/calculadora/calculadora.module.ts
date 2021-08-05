2bdf64741d3e592d13c3e86d24de07000ab2aa0f
Marcos <saoroque@hotmail.com>
2021-08-04 22:56:51 -0300

    Calculadora

---
+++ src/app/calculadora/calculadora.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalculadoraComponent } from './components';
+import { CalculadoraService } from './services';
+
+
+
+@NgModule({
+  declarations: [
+    CalculadoraComponent
+  ],
+  exports: [
+    CalculadoraComponent
+  ],
+  providers: [
+    CalculadoraService
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CalculadoraModule { }
